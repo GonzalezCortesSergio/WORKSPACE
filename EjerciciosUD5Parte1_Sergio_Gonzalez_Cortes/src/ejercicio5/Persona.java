c2c3b0e68c55930e8fd737a6c8e057f3302d982a
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-02-15 12:29:37 +0100

    Ejercicio 5 empezado

---
+++ EjerciciosUD5Parte1_Sergio_Gonzalez_Cortes/src/ejercicio5/Persona.java
@@ -0,0 +1,53 @@
+package ejercicio5;
+
+public class Persona {
+
+	
+	//Atributos
+	
+	private String nombre;
+	private int edad;
+	
+	
+	//Constructor
+	
+	public Persona(String nombre, int edad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+	}
+
+
+	
+	//Getters and Setters
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	
+	//toString
+	
+	@Override
+	public String toString() {
+		return "Persona [nombre=" + nombre + ", edad=" + edad + "]";
+	}
+	
+	
+	
+}
