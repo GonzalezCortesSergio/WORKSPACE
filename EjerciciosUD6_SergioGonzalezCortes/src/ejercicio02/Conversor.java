8858b614ebbaf185547778759844933bf0aabd7e
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-03-18 12:30:20 +0100

    Ejercicio 2 hecho

---
+++ EjerciciosUD6_SergioGonzalezCortes/src/ejercicio02/Conversor.java
@@ -0,0 +1,29 @@
+package ejercicio02;
+
+public class Conversor {
+
+	//Métodos
+	
+	public double convertirAFahrenheit (double gradosCelsius) throws TempValueException{
+	
+		if (gradosCelsius <= -273) {
+			throw new TempValueException();
+		}
+		
+		return (gradosCelsius * (9/5)) + 32;
+	
+	}
+	
+	public double convertirACelsius (double gradosFahrenheit) throws TempValueException {
+		
+		double gradosCelsius;
+		
+		gradosCelsius = (gradosFahrenheit - 32) * 5/9;
+		
+		if (gradosCelsius <= -273) {
+			throw new TempValueException();
+		}
+		
+		return gradosCelsius;
+	}
+}
