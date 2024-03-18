5da47e0fdce3aac69e1850a3e753099d4808fa70
Sergio González Cortés <gonzalez.coser24@triana.salesianos.edu>
2024-03-18 11:50:15 +0100

    Ejercicio 1 cambiando cositas (en verdad no)

---
+++ EjerciciosUD6_SergioGonzalezCortes/src/ejercicio01b/Calculadora.java
@@ -0,0 +1,17 @@
+package ejercicio01b;
+
+public class Calculadora {
+
+	
+	//Métodos
+	
+	public int dividir (int num1, int num2) {
+		
+		return num1 / num2;
+	}
+	
+	public double raizCuadrada (int num1) {
+		
+		return Math.sqrt(num1);
+	}
+}
---
+++ EjerciciosUD6_SergioGonzalezCortes/src/ejercicio01b/Principal.java
@@ -0,0 +1,41 @@
+package ejercicio01b;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Principal {
+
+	
+	public static void main (String [] args) {
+		
+		int numero1, numero2;
+		String aux;
+		Calculadora c = new Calculadora();
+		Scanner s = new Scanner(System.in);
+		try {
+			
+			System.out.println("Introduzca dos números");
+			aux = s.nextLine();
+			numero1 = Integer.parseInt(aux);
+			aux = s.nextLine();
+			numero2 = Integer.parseInt(aux);
+			
+			System.out.println(c.dividir(numero1, numero2)
+					);
+			
+			System.out.println(c.raizCuadrada(numero2));
+			
+			
+		}catch (ArithmeticException e) {
+			
+			System.out.println("El denominador no puede ser cero");
+		}catch (InputMismatchException e) {
+			
+			System.out.println("Introduzca un número entero");
+		}catch (Exception e) {
+			
+			System.out.println("Error inesperado");
+		}
+		
+	}
+}
